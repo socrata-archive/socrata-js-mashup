d863bfeb79ee3ba4dadb348fb62348af3ee9db9c
Aiden Scandella <aiden.scandella@socrata.com>
2010-03-02 15:23:43 -0800

    Checkpoint: jsonp callback now working

--- map.js
+++ map.js
@@ -1,15 +1,23 @@
 $( function() {
-    var mapDataset = new Dataset();
+    $.ajaxSetup({"error":function(XMLHttpRequest,textStatus, errorThrown) {   
+        alert(textStatus);
+        alert(errorThrown);
+        alert(XMLHttpRequest.responseText);
+    }});
+    
+    
+    var dataset = new Dataset();
     $("#datasetForm").submit( function(e) {
         // Don't actually submit the form
         e.preventDefault();
         
         datasetLocation = $("#datasetForm #location").val();
         // Look for a UID and host name
-        mapDataset.extractUID(datasetLocation);
-        mapDataset.extractHost(datasetLocation);
+        dataset.extractUID(datasetLocation);
+        dataset.extractHost(datasetLocation);
         
-        alert(mapDataset.host);
-        alert(mapDataset.uid);
+        // Get an array of columns
+        console.log(dataset.columnsURL());
+        $.getJSON(dataset.columnsURL(), dataset.columnsCallback);
     });
 });
--- socrata.js
+++ socrata.js
@@ -16,29 +16,40 @@ Dataset.prototype.extractUID = function(url) {
 
 // TODO: Better protocol handling
 Dataset.prototype.extractHost = function(url) {
-    matches = url.match(/^(?:[^\/]+:\/\/)?([^\/:]+)/im);
+    matches = url.match(/^(?:[^\/]+:\/\/)?([^\/]+)/im);
     if ( matches == null || matches.length < 2 ) {
         return;
     }
     this.host = "http://" + matches[1];
-}
+};
+
+Dataset.prototype.columnsCallback = function(data) {
+    console.log(data);
+    this.columns = data;
+};
+
+// Ready a string for use in JSONP callback
+Dataset.prototype.jsonWrap = function(url) {
+    return this.host + url + 
+        (url.indexOf('?') == -1 ? '?' : '&') + 'jsonp=?';
+};
 
 // Where to get general information about this dataset
 Dataset.prototype.viewDataURL = function() {
-    return this.host + "/views/" + this.uid + ".json"; 
-}
+    return this.jsonWrap("/views/" + this.uid);
+};
 
 // Where to get the rows JSON from
 Dataset.prototype.rowsURL = function() {
-    return this.host + "/views/" + this.uid + "/rows.json";
+    return this.jsonWrap("/views/" + this.uid + "/rows.json");
 };
 
 // And the columns
 Dataset.prototype.columnsURL = function() {
-    return this.host + "/views/" + this.uid + "/columns.json";
+    return this.jsonWrap("/views/" + this.uid + "/columns.json");
 };
 
 // A short link to this dataset
 Dataset.prototype.shortURL = function() {
     return this.host + "/d/" + this.uid;
-}
+};
